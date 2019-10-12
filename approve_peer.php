982c100b3a923654b19be3122177399bacced607
Chris <christopher@ve7alb.ca>
2019-10-11 20:39:49 -0700

    changed starting IP range to not conflict with additional ips

--- approve_peer.php
+++ approve_peer.php
@@ -47,7 +47,7 @@
 	}
 
 	//find next available IP address
-	for($i=3461441638; $i< 3461441776; $i++)
+	for($i=3461441656; $i< 3461441776; $i++)
 	{
 		$query="SELECT * FROM peers WHERE address=". $i;
 		$rows=mysqli_num_rows(mysqli_query($conn,$query));
