a28aeda42177ff66a77f91530ee19e92d3c88b8a
Bryce Wilson <bryce@brycemw.ca>
2020-09-01 17:42:45 +1200

    Fixed bugs

--- scripts/hostname.sh
+++ scripts/hostname.sh
@@ -6,15 +6,15 @@ ips=`ip -br addr | egrep -o -e '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' 
 
 host=""
 
-for host in $hosts; do
-  exec 6< "$host"
+for hoststring in $hosts; do
+  exec 6< "$hoststring"
   read -r name <&6
   read -r hostname <&6
   read -r port <&6
 
   for ip in $ips; do
     if [ $ip == "`dig $hostname +short`" ]; then
-      host=$host
+      host=$hoststring
     fi
   done
 
