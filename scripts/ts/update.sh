18dd9097c4ce922f53c85b2a2d6610697c15b0d6
Bryce Wilson <bryce@brycemw.ca>
2020-08-31 22:25:42 -0700

    Finished update script

--- scripts/ts/update.sh
+++ scripts/ts/update.sh
@@ -2,4 +2,10 @@ host=`/evix/scripts/hostname.sh`
 
 git fetch
 
-git diff --name-only bryce-update...origin/bryce-update | grep "/evix/config/peers/$host" | cut -d '.' -f1 | xargs bash -c "/evix/scripts/$0"
+updates=(`git diff --name-only bryce-update...origin/bryce-update | grep "config/peers/$host" | cut -d '.' -f2`)
+
+git merge origin/master
+
+for update in $updates; do
+  /evix/scripts/ts/$update
+done
