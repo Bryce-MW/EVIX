dac1b636c49fee23002cb1116d1d365a0e46b3cf
Bryce Wilson <bryce@brycemw.ca>
2020-09-01 18:01:08 +1200

    Fixed bugs

--- scripts/hostname.sh
+++ scripts/hostname.sh
@@ -2,18 +2,20 @@
 
 hosts=("/evix/config/hosts"/*)
 
-ips=`ip -br addr | egrep -o -e '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -e '([a-f0-9:]+:+)+[a-f0-9]+'`
+ips=(`ip -br addr | egrep -o -e '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -e '([a-f0-9:]+:+)+[a-f0-9]+'`)
 
 host=""
 
-for hoststring in $hosts; do
+for hoststring in ${hosts[@]}; do
   exec 6< "$hoststring"
   read -r name <&6
   read -r hostname <&6
   read -r port <&6
 
-  for ip in $ips; do
-    if [ $ip == "`dig $hostname +short`" ]; then
+  hostip="`dig $hostname +short`"
+
+  for ip in ${ips[@]}; do
+    if [ $ip == "$hostip" ]; then
       host=$hoststring
     fi
   done
@@ -23,3 +25,5 @@ done
 if [ host == "" ]; then
   exit -1
 fi
+
+basename $host
