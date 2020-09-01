b35942bbcc49b9d50ceef659bb6085d6db842cc8
Bryce Wilson <bryce@brycemw.ca>
2020-08-31 22:13:47 -0700

    Updated update script

--- scripts/ts/update.sh
+++ scripts/ts/update.sh
@@ -2,4 +2,4 @@ host=`/evix/scripts/hostname.sh`
 
 git fetch
 
-git diff --name-only bryce-update...origin/bryce-update | grep '\.peers' | cut -d '.' -f1 | xargs bash -c "/evix/scripts/$0"
+git diff --name-only bryce-update...origin/bryce-update | grep "/evix/config/peers/$host" | cut -d '.' -f1 | xargs bash -c "/evix/scripts/$0"
