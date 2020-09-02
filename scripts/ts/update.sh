327de8f4f1f582abb37b4fd550f7aa071af41332
Bryce Wilson <bryce@brycemw.ca>
2020-09-01 23:06:14 -0700

    Allowed for autmoatic banch changing

--- scripts/ts/update.sh
+++ scripts/ts/update.sh
@@ -1,10 +1,12 @@
 host=`/evix/scripts/hostname.sh`
 
+branch=`git branch --show-current`
+
 git fetch
 
-updates=(`git diff --name-only bryce-update...origin/bryce-update | grep "config/peers/$host" | cut -d '.' -f2`)
+updates=(`git diff --name-only $branch...origin/$branch | grep "config/peers/$host" | cut -d '.' -f2`)
 
-git merge origin/bryce-update
+git merge origin/$branch
 
 for update in $updates; do
   /evix/scripts/ts/$update.sh
