2eb1266377744ecf02b7c5e420e8ea32378a76ec
Bryce Wilson <bryce@brycemw.ca>
2020-09-13 20:12:31 -0700

    Finished interaction script

--- scripts/root/peers-status.py
+++ scripts/root/peers-status.py
@@ -37,7 +37,7 @@ check which one this email is being sent about!
 """
 
 try:
-	database = mysql.connector.connect(user='evix', password='***REMOVED***', host='127.0.0.1', database='evix')
+	database = mysql.connector.connect(user='evix', password='***REMOVED***', host='127.0.0.1', database='evix', autocommit=True)
 except mysql.connector.Error as err:
 	print("Something went wrong with the database connection:")
 	print(err)
@@ -66,17 +66,22 @@ with smtplib.SMTP_SSL("***REMOVED***", 465, context=context) as server:
 		since = datetime.datetime.strptime(" ".join(line[1:3]), "%Y-%m-%d %H:%M:%S")
 		difference = (now - since).days
 		if difference == 3 or difference >= 14:
-			cursor.execute("SELECT name,contact,provisioned FROM clients INNER JOIN asns ON client_id=id INNER JOIN ips ON ips.asn=asns.asn WHERE ip=%s AND monitor=true", (ip,))
+			cursor.execute("SELECT name,contact,provisioned FROM clients INNER JOIN asns ON client_id=id INNER JOIN ips ON ips.asn=asns.asn WHERE ip=%s AND monitor=true AND provisioned=true", (ip,))
 			results = tuple(cursor)
 			if len(results) == 0:
-				print(f"No result found for {asn} over {ip} (not monitored?)")
+				print(f"- No result found for {asn} over {ip} (not monitored or deprovisioned?)")
 				continue
 			name, contact, provisioned = results[0]
+			if not contact:
+				print(f"Error: {name} has no email, not sending email for {asn} over {ip}")
+				continue
 			if difference >= 14 and provisioned:
 				cursor.execute("UPDATE ips SET provisioned=false WHERE ip=%s", (ip,))
+				if cursor.rowcount == 0:
+					print(f"- Could not deprovision {ip}?")
+				results = tuple(cursor)
 				server.sendmail("support@evix.org", (contact,"peering@evix.org"), email.format(name=name, version=version, difference=difference, asn=asn, ip=ip, server=rt, contact=contact))
 				print(f"Sent deprovison for {asn} over {ip} to {contact}")
 			elif difference == 3 and provisioned:
 				server.sendmail("support@evix.org", (contact,"peering@evix.org"), email.format(name=name, version=version, difference=difference, asn=asn, ip=ip, server=rt, contact=contact))
 				print(f"Sent warning for {asn} over {ip} to {contact}")
-	server.quit()
