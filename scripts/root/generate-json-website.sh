8ad7891dbb427b433885602ec866c5d08cb8080e
Chris <christopher@ve7alb.ca>
2020-09-13 23:09:52 -0700

    Added back script

---
+++ scripts/root/generate-json-website.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JSON=`/usr/bin/php /evix/scripts/peers-json.php`
+
+#if config is valid, reload bird
+if [ $? -eq 0 ];then
+  echo Exit Code OK, updating website
+  echo '' >  /var/www/evix/participants.json
+  echo $JSON > /var/www/evix/participants.json
+else
+  echo Exit code NOT ok, NOT updating website
+fi
