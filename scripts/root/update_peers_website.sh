04cd42e74f8e3a75a491d4de4677cd748d5345c5
Chris <christopher@ve7alb.ca>
2020-09-18 10:50:02 -0700

    Updated peers (script)

--- scripts/root/update_peers_website.sh
+++ scripts/root/update_peers_website.sh
@@ -11,13 +11,13 @@ else
   new=($(md5sum /tmp/ix_peers.html))
 #  old=($(md5sum /evix/run/IX-Website/templates/page/ix_peers.html))
   echo $new
-  echo $old
+#  echo $old
   #if file is new
-  if [ "$new" != "$old" ];then
+#  if [ "$new" != "$old" ];then
     cp /tmp/ix_peers.html /evix/run/IX-Website/templates/page/ix_peers.html
     cd /evix/run/IX-Website/
-    git commit -a -m "Updated website (script)"
-    git push
-    /usr/local/bin/staticjinja build --srcpath=templates --static=static --outpath=/var/www/evix/ --globals=globals.yaml
+#    git commit -a -m "Updated website (script)"
+#    git push
+#    /usr/local/bin/staticjinja build --srcpath=templates --static=static --outpath=/var/www/evix/ --globals=globals.yaml
   fi
 fi
