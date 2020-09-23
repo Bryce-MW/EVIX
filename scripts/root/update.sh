1e68fa8a2ce6aaca7122cd2548199ec2a8e95312
Chris <christopher@ve7alb.ca>
2020-09-23 12:30:45 -0700

    Updated update script

--- scripts/root/update.sh
+++ scripts/root/update.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
 cd /evix/config/peers
+git add --all
+git stash
+git checkout master
+git pull
+git stash pop
 git commit -a -m "Updated peers (script)"
 git push
 
 cd /evix/config/ccd
 git add --all
-git commit -a -m "Updated OpenVPN peers (script)"cd /evix/config/peers
-git commit -a -m "Updated peers (script)"
-git push
-
-cd /evix/config/ccd
-git add --all
+git stash
+git checkout master
+git pull
+git stash pop
 git commit -a -m "Updated OpenVPN peers (script)"
 
 cd /evix
