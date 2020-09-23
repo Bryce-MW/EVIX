3ce7011a3cc20a641644f956401074833b3cbe18
Bryce Wilson <bryce@brycemw.ca>
2020-09-22 21:11:05 -0700

    updated fmt reboot

--- config/reboot/fmt.sh
+++ config/reboot/fmt.sh
@@ -1,7 +1,7 @@
 ip -4 route delete 206.81.104.0/24
 ip -4 route add 206.81.104.0/24 dev vmbr0 src 206.81.104.1 pref high
 ip addr add 2602:fed2:fc0:c8::1/44 dev ens18
-ip -6 route add ::/0 via 2602:fed2:fc0:c0::1
+ip -6 route add ::/0 via 2602:fed2:fc0::1
 
 
 ip link add vtep123 vxlan id 123 remote 104.218.61.207 local 72.52.82.6
