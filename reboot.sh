dc78d1f84744fc472d19d5dcfc2c813a1a27938b
Bryce Wilson <bryce@brycemw.ca>
2020-11-28 00:56:10 -0800

    fix more perms

--- reboot.sh
+++ reboot.sh
@@ -6,7 +6,7 @@ git -C /evix submodule update --init --recursive
 
 if [ "$(/evix/scripts/get-val.sh "$host" is-ts)" == "true" ]; then
 
-  /evix/scripts/ts/vxlan.sh
+  /evix/scripts/ts/tunnels/vxlan.sh
   /evix/scripts/ts/eoip-new.sh
 
   xargs -n 1 ovs-vsctl add-port vmbr0 </evix/config/ports/"$host".ports
