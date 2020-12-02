016664b3879090ed1068ae87452a67a49d72b0c5
Bryce Wilson <bryce@brycemw.ca>
2020-12-01 19:52:53 -0800

    Add IPs

--- config/reboot/ams.sh
+++ config/reboot/ams.sh
@@ -1,2 +1,5 @@
+ip -4 address add 206.81.104.253/24 dev br10
+ip -6 address add 2602:fed2:fff:ffff::253/64 dev br10
+
 #ip link add vtep1234 type vxlan id 1234 local 93.158.213.143 remote 72.52.82.6 dstport 500
 #ip link set up vtep1234
--- config/reboot/fmt.sh
+++ config/reboot/fmt.sh
@@ -1,5 +1,6 @@
 ip -4 route delete 206.81.104.0/24
-ip -4 route add 206.81.104.0/24 dev br10 src 206.81.104.1 pref high
+ip -4 address add 206.81.104.1/24 dev br10
+ip -6 address add 2602:fed2:fff:ffff::1/64 dev br10
 ip addr add 2602:fed2:fc0:c8::1/44 dev ens18
 ip -6 route add ::/0 via 2602:fed2:fc0::1
 
