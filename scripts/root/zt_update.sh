dc1ff0e334a66c43320b47623ed39abe2bdcd140
Chris <christopher@ve7alb.ca>
2021-02-13 00:48:47 -0800

    Started automated Zerotier1

--- scripts/root/zt_update.sh
+++ scripts/root/zt_update.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-mysql --user evix --password=***REMOVED*** evix <<<"SELECT ip, additional_args FROM connections WHERE type='zerotier' AND EXISTS (SELECT 1 FROM clients INNER JOIN asns ON clients.id=asns.client_id INNER JOIN ips ON ips.asn=asns.asn WHERE provisioned=true AND clients.id=connections.client_id);"
+mysql --user evix --password=***REMOVED*** evix <<<"SELECT ip, additional_args FROM connections WHERE type='zerotier';"
+
+
+# AND EXISTS (SELECT 1 FROM clients INNER JOIN asns ON clients.id=asns.client_id INNER JOIN ips ON ips.asn=asns.asn WHERE provisioned=true AND clients.id=connections.client_id);"
