25384a1acf7b49cf85be72d4f0d8070c0a034e09
Chris <christopher@ve7alb.ca>
2021-02-15 22:44:50 -0800

    Fix fmt reboot

--- config/reboot/fmt.sh
+++ config/reboot/fmt.sh
@@ -8,6 +8,9 @@ ip route add 206.81.104.0/24 dev br10
 ip link add EVIX-VAN type vxlan id 11 local 72.52.82.6 remote 104.218.61.207 dstport 5000 learning rsc
 ip link set up EVIX-VAN
 
+ip route delete 2602:fed2:fff:ffff::/64 dev ens19
+ip route delete 2602:fed2:fff:ffff::/64 dev EVIX-VAN
+
 #ip link add vtep123 vxlan id 123 remote 104.218.61.207 local 72.52.82.6
 #ip link add vtep1234 vxlan id 1234 remote 93.158.213.143 local 72.52.82.6 srcport 0 0 dstport 500 ageing 300
 #ip link set vtep123 up
