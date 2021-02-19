c1c1581f1bb9309ae8756bdf483371109e0ef058
Bryce Wilson <bryce@brycemw.ca>
2021-02-18 21:43:31 -0800

    Add comment tp brycerc

--- config/brycerc.sh
+++ config/brycerc.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+# NOTE(bryce): Written by Bryce Wilson a while ago and added to git on 2021-02-14.
+#  * 2021-12-18|>Bryce|>Add alias for ip to add color
+
 if ! command -v bat &> /dev/null; then
   echo "Please install bat https://github.com/sharkdp/bat"
 else
