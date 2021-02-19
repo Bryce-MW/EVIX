91289b39ba1dcf6dc2b593d40c18844443830006
Bryce Wilson <bryce@brycemw.ca>
2021-02-19 13:09:38 -0800

    AFixed minor bugs

--- scripts/root/tunnel_server_config_generate.py
+++ scripts/root/tunnel_server_config_generate.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 # NOTE(bryce): Written by Bryce Wilson on 2020-09-13, migrated from tunnel_server_config_generate.sh (prior known as
-#  gen-config
+#  gen-config)
 #  * 2020-09-14|>Bryce|>Migrated from old file
 #  * 2020-12-15|>Bryce|>Renamed from gen-config.py to tunnel_server_config_generate.py
+#  * 2021-02-19|>Bryce|>Fixed minor bugs
 
 import mysql.connector
 
@@ -21,13 +22,13 @@ config = {}
 cursor.execute("SELECT server,type,tunnel_id,ip,additional_args,client_id FROM connections")
 
 for i in cursor:
-	server, type, id, ip, additional_args, client_id = i
-	if type == "zerotier":
+	server, tunnel_type, tunnel_id, ip, additional_args, client_id = i
+	if tunnel_type == "zerotier":
 		config[f"zerotier.peers"] = config.get(f"zerotier.peers", "") + f"{ip} {additional_args}\n"
-	elif type == "openvpn":
+	elif tunnel_type == "openvpn":
 		config[f"openvpn.peers"] = config.get(f"openvpn.peers", "") + f"{ip} {client_id} {additional_args}\n"
 	else:
-		config[f"{server}.{type}"] = config.get(f"{server}.{type}", "") + f"{id} {ip} {additional_args}\n"
+		config[f"{server}.{tunnel_type}"] = config.get(f"{server}.{tunnel_type}", "") + f"{tunnel_id} {ip} {additional_args}\n"
 
 for i in config:
 	with open(f"/evix/config/peers/{i}", "w") as file:
