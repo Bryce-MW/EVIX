3f968d646253548dd07f112f6387502527188fb4
Bryce Wilson <bryce@brycemw.ca>
2021-02-19 13:25:13 -0800

    Add comment and slight chang

--- scripts/get-val.sh
+++ scripts/get-val.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+# NOTE(bryce): Written by Bryce Wilson on 2020-08-30
+#  * 2020-11-28|>Bryce|>Minor fix of syntax
 
 host=$1
 key=$2
 
-grep "$key=" /evix/config/key-value/"$host".conf | cut -d '=' -f2
+grep "$key=" "/evix/config/key-value/$host.conf" | cut -d '=' -f2
