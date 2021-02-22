b56d338414ac94b1cfee06febdabcb45839cc016
Bryce Wilson <bryce@brycemw.ca>
2021-02-21 20:49:36 -0800

    Test new bird configure

--- scripts/root/bird_configure.sh
+++ scripts/root/bird_configure.sh
@@ -20,50 +20,45 @@ fi
 PYTHONPATH="$(pwd)"
 export PYTHONPATH
 
-#generate yaml from database
-#PEERS=$(/usr/bin/php /evix/scripts/root/bird_configure.php)
 mv /etc/arouteserver/clients.yml /etc/arouteserver/clients.yml.bak
-#echo "$PEERS" >/etc/arouteserver/clients.yml
-/evix/run/arouteserver/scripts/arouteserver clients-from-euroix 756 -i /var/www/evix/participants.json --guess-custom-bgp-communities switch_name --merge-from-peeringdb as-set max-prefix -o /evix/config/arouteserver/clients.yml
+/evix/run/arouteserver/scripts/arouteserver clients-from-euroix 756 -i /var/www/evix/participants.json --guess-custom-bgp-communities switch_name --merge-from-peeringdb as-set max-prefix -o /evix/run/arouteserver/clients.yml
 
-/evix/run/arouteserver/scripts/arouteserver bird --ip-ver 4 --local-files-dir /etc/bird --use-local-files header -o /tmp/bird.conf
-/evix/run/arouteserver/scripts/arouteserver bird --ip-ver 6 --local-files-dir /etc/bird --use-local-files header -o /tmp/bird6.conf
+new=$(md5sum /etc/arouteserver/clients.yml | cut -f1 -d' ') # I don't understand why cut needs to be required. Someone should put in a pull request to allow --quiet to remove names
+old=$(md5sum /etc/arouteserver/clients.yml.bak | cut -f1 -d' ')
+if [ "$new" != "$old" ]; then
+  /evix/run/arouteserver/scripts/arouteserver bird --target-version 1.6.8 --ip-ver 4 --local-files-dir /etc/bird --use-local-files header -o /tmp/bird.conf
+  /evix/run/arouteserver/scripts/arouteserver bird --target-version 1.6.8 --ip-ver 6 --local-files-dir /etc/bird --use-local-files header -o /tmp/bird6.conf
+else
+  exit 0
+fi
 
 SUCCESS=1
 
-new=$(md5sum /tmp/bird.conf)
-old=$(md5sum /evix/config/bird/bird.conf)
+new=$(md5sum /tmp/bird.conf | cut -f1 -d' ')
+old=$(md5sum /evix/config/bird/bird.conf | cut -f1 -d' ')
+new6=$(md5sum /tmp/bird6.conf | cut -f1 -d' ')
+old6=$(md5sum /evix/config/bird/bird6.conf | cut -f1 -d' ')
 
 #if file is new
-if [ "$new" != "$old" ]; then
-  echo md5 has changed... updating bird
+if [ "$new" != "$old" ] || [ "$new6" != "$old6" ]; then
+  echo "MD5 has changed... updating bird"
+  ansible-playbook /evix/config/playbooks/local_bird.yml
 
   #if config is valid, reload bird
-  if /usr/sbin/bird -p -c /tmp/bird.conf; then
-    echo bird configuration is valid
+  if /usr/sbin/bird -p -c /tmp/bird_local.conf; then
+    echo "Bird configuration is valid"
     mv /tmp/bird.conf /evix/config/bird/
-    sed -i -e 's/rs_as = 137933/rs_as = {{ rs_asn }}/g' /evix/config/bird/bird.conf
-    sed -i -e 's/local as 137933/local as {{ rs_asn }}/g' /evix/config/bird/bird.conf
   else
-    echo Bird configuration is invalid
+    echo "Bird configuration is invalid"
     SUCCESS=0
   fi
-fi
-
-new=$(md5sum /tmp/bird6.conf | cut -f1 -d' ')
-old=$(md5sum /evix/config/bird/bird6.conf | cut -f1 -d' ')
-
-#if file is new
-if [ "$new" != "$old" ]; then
 
   #if config is valid, reload bird
-  if /usr/sbin/bird6 -p -c /tmp/bird6.conf; then
-    echo bird 6 configuration is valid
+  if /usr/sbin/bird6 -p -c /tmp/bird6_local.conf; then
+    echo "Bird6 configuration is valid"
     mv /tmp/bird6.conf /evix/config/bird/
-    sed -i -e 's/rs_as = 137933/rs_as = {{ rs_asn }}/g' /evix/config/bird/bird6.conf
-    sed -i -e 's/local as 137933/local as {{ rs_asn }}/g' /evix/config/bird/bird6.conf
   else
-    echo Bird 6 configuration is invalid
+    echo "Bird6 configuration is invalid"
     SUCCESS=0
   fi
 fi
@@ -71,5 +66,5 @@ fi
 if [ "$SUCCESS" == "0" ]; then
   echo "Config failed"
 else
-  /usr/bin/ansible-playbook /evix/config/playbooks/push_bird.yml
+  ansible-playbook /evix/config/playbooks/push_bird.yml
 fi
