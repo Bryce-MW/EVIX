8d81b131d2354a988abc106fb3b5ef352d430d0a
Alexander Brügmann <mail+evix@abruegmann.eu>
2021-02-28 18:21:34 +0100

    fix config parsing

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -8,7 +8,10 @@ WEBHOOK_URL="***REMOVED***"
 
 host=$(/evix/scripts/hostname.sh)
 bridge="br10"
-source "/evix/config/key-value/$host.conf"
+
+is-ts=$(/evix/scripts/get-val.sh "$host" is-ts)
+is-rs=$(/evix/scripts/get-val.sh "$host" is-rs)
+is-admin=$(/evix/scripts/get-val.sh "$host" is-admin)
 
 # Let's be careful
 alias rm='rm -I'
@@ -31,13 +34,13 @@ mkdir -p "$STATE_FILE_DIR"
 #
 # Are all our services running?
 #
-for service in ${services[@]}; do
+for service in $services; do
   state_file="$STATE_FILE_DIR/$service.not.running"
   systemctl is-active --quiet "$service"
   if [ $? -ne 0 -a ! -f "$state_file" ]; then
     send_alert "Service $service is not running."
     touch "$state_file" 
-  elif [ -f "$state_file" ]
+  elif [ -f "$state_file" ]; then
     rm "$state_file"
   fi
 done
@@ -49,7 +52,7 @@ if [ "$is-ts" = "true" ]; then
   if [ "$eoip_ps" == "" -a ! -f "$state_file" ]; then
     send_alert "eoip is not running."
     touch "$state_file" 
-  elif [ -f "$state_file" ]
+  elif [ -f "$state_file" ]; then
     rm "$state_file"
   fi
 fi
@@ -60,12 +63,15 @@ fi
 if [ "$is-ts" = "true" ]; then
   vxlan-interfaces=$(basename -a /sys/class/net/vtep*)
   backbone-interfaces=$(basename -a /sys/class/net/EVIX*)
+  eoip-interface=$(/evix/scripts/get-val.sh "$host" eoip-interface)
+  ovpn-interface=$(/evix/scripts/get-val.sh "$host" ovpn-interface)
+  zt-interface=$(/evix/scripts/get-val.sh "$host" zt-interface)
   for i in $vxlan-interfaces $backbone-interfaces $eoip-interface $ovpn-interface $zt-interface; do
     state_file="$STATE_FILE_DIR/$i.not.on.bridge"
     if [[ "$(readlink /sys/class/net/$i/brport/bridge)" != *br10 ]] && [ ! -f "$state_file" ]; then
       send_alert "$i is not added to $bridge."
       touch "$state_file" 
-    elif [ -f "$state_file" ]
+    elif [ -f "$state_file" ]; then
       rm "$state_file"
     fi
   done
