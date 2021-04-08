d9cf5574cdd556555e251515b4d8c34bcfc7d609
Alexander Brügmann <mail+evix@abruegmann.eu>
2021-04-08 14:02:37 +0200

    restart smokeping if its config has been updated

--- scripts/root/smokeping_targets_generate.sh
+++ scripts/root/smokeping_targets_generate.sh
@@ -20,6 +20,7 @@ newconf="$TMPDIR/$date_$CONF_FILE_NAME"
 if ! cmp -s "$oldconf" "$newconf"; then
   mv -f "$newconf" "$oldconf"
   touch "$SMOKEPING_CONF_DIR/../config"
+  /bin/systemctl restart smokeping.service
 else
   rm "$newconf"
 fi
