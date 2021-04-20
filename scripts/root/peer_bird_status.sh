802621f817da6dfe7b9ce9bd5195ea82517bb401
Bryce Wilson <bryce@brycemw.ca>
2021-04-19 21:49:32 -0700

    Start work on fixing peer status

--- scripts/root/peer_bird_status.sh
+++ scripts/root/peer_bird_status.sh
@@ -9,8 +9,8 @@ jq -r --compact-output '.hosts[] | select(.roles | contains(["rs"])) | {name, ho
     hostname=$(jq -r '.hostname' <<<"$line")
     port=$(jq -r '.ssh_port' <<<"$line")
     ssh -n -p "$port" "$hostname" birdc show protocols all | tail -n +3 | head -n -1 |
-      jq --slurp --raw-input --compact-output --var version 4 --var server "$name" '{version: $version, server: $server, status: parse_bird}'
+      jq --slurp --raw-input --compact-output --arg version 4 --arg server "$name" '{version: $version, server: $server, status: parse_bird}'
     ssh -n -p "$port" "$hostname" birdc6 show protocols all | tail -n +3 | head -n -1 |
-      jq --slurp --raw-input --compact-output --var version 6 --var server "$name" '{version: $version, server: $server, status: parse_bird}'
+      jq --slurp --raw-input --compact-output --arg version 6 --arg server "$name" '{version: $version, server: $server, status: parse_bird}'
   done #|
   #python3 /evix/scripts/root/warn_disconnection.py 4
