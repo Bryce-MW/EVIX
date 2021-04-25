bff5d1b5472ad286f44be773cd01fbaa3852d92a
Bryce Wilson <bryce@brycemw.ca>
2021-04-25 14:09:02 -0700

    cp correctly

--- run/website/render.sh
+++ run/website/render.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /var/www/evix/new
 rm -rf /var/www/evix/new/*
-cp -r /evix/run/website /var/www/evix/new
+cp -r /evix/run/website/* /var/www/evix/new/
 cd /var/www/evix/new || exit
 sass ./main.scss
