6f953319d47cbf5c8b95be9fcf2bc229525c3c8e
Bryce Wilson <bryce@brycemw.ca>
2021-04-25 22:04:23 -0700

    Have rsync do its job

--- run/website/render.sh
+++ run/website/render.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 /usr/bin/sass /evix/run/website/main.scss /evix/run/website/main.css
-/usr/bin/rsync -r /evix/run/website /var/www/evix/new
+/usr/bin/rsync -r -v --delete --del /evix/run/website/ /var/www/evix/new/
