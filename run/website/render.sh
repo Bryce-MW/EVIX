0f0d7cf7e9acbb97923ce7890693291c7c7fee3c
Bryce Wilson <bryce@brycemw.ca>
2021-05-05 22:45:18 -0700

    Exclude graphs from being overwritten

--- run/website/render.sh
+++ run/website/render.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 /usr/bin/sass /evix/run/website
-/usr/bin/rsync -r -v --delete --del /evix/run/website/ /var/www/evix/new/
+/usr/bin/rsync -r -v --delete --del /evix/run/website/ /var/www/evix/new/ --exclude=static/graphs
