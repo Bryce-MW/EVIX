6adccb1ae66e89fecce733460edec228d66e4fbf
Chris <christopher@ve7alb.ca>
2021-05-14 10:02:37 -0700

    Add render to script

--- run/website/render.sh
+++ run/website/render.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 /usr/bin/sass /evix/run/website
+/evix/run/website/peers.py > /evix/run/website/peers.html
 /usr/bin/rsync -r -v --delete --del /evix/run/website/ /var/www/evix/new/ --exclude=static/graphs
