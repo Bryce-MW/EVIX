8853b7175ef8f2142e1cacf7b968aca10ef99a18
Bryce Wilson <bryce@brycemw.ca>
2021-05-13 23:25:42 -0700

    Make the string nicer

---
+++ run/website/peers.jq
@@ -0,0 +1,22 @@
+reduce
+  .[]
+as $item
+  ([];
+  if
+    (. | map(select(.asn == $item.asn)) | length > 0)
+  then
+    map(if .asn == $item.asn then .additional_ips += [$item.ip] else . end)
+  else . + [$item + {additional_ips:[]}]
+  end)
+| reduce
+  .[]
+as $item
+  ([];
+  if
+    (. | map(select(.client_id == $item.client_id)) | length > 0)
+  then
+    map(if .client_id == $item.client_id then .additional_asns += [$item | {ip, asn, additional_ips}] else . end)
+  else
+    . + [$item + {additional_asns:[]}]
+  end)
+| .[]
--- run/website/peers.py
+++ run/website/peers.py
@@ -56,7 +56,6 @@ if __name__ == "__main__":
             IS_IPV6(ip),
             INET6_ATON(ip)
     """)
-    print(jq.compile("""
-        reduce .[] as $item([]; if (. | map(select(.asn == $item.asn)) | length > 0) then map(if .asn == $item.asn then .additonal_ips += [$item.ip] else . end) else . + [$item + {additional_ips:[]}] end) | reduce .[] as $item([]; if (. | map(select(.client_id == $item.client_id)) | length > 0) then map(if .client_id == $item.client_id then .additional_asns += [$item | {ip, asn, additional_ips}] else . end) else . + [$item + {additional_asns:[]}] end) | .[]
-    """).input(text="[" + ",".join(i[0] for i in cursor.fetchall()) + "]").first())
+    with open('peers.jq', 'r') as jq_script:
+        print(jq.compile(jq_script.read()).input(text="[" + ",".join(i[0] for i in cursor.fetchall()) + "]").first())
 
