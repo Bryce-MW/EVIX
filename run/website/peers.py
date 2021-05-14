d8687575a9cc406d75f6400cffdf74a3ba192352
Bryce Wilson <bryce@brycemw.ca>
2021-05-13 23:14:42 -0700

    Next test

--- run/website/peers.py
+++ run/website/peers.py
@@ -57,6 +57,6 @@ if __name__ == "__main__":
             INET6_ATON(ip)
     """)
     print(jq.compile("""
-        .
+        reduce .[] as $item([]; if (. | map(select(.asn == $item.asn)) | length > 0) then map(if .asn == $item.asn then .additonal_ips += [$item.ip] else . end) else . + [$item + {additional_ips:[]}] end) | reduce .[] as $item([]; if (. | map(select(.client_id == $item.client_id)) | length > 0) then map(if .client_id == $item.client_id then .additional_asns += [$item | {ip, asn, additional_ips}] else . end) else . + [$item + {additional_asns:[]}] end) | .[]
     """).input("[" + ",".join(i[0] for i in cursor.fetchall()) + "]").first())
 
