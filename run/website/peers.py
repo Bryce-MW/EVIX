993784385a42c143eb7bb7f8178697e284c56269
Bryce Wilson <bryce@brycemw.ca>
2021-05-13 23:03:47 -0700

    Start work on peers

---
+++ run/website/peers.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+import os
+
+import json
+import jq
+import mysql.connector
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+if "REQUEST_METHOD" in os.environ and __name__ == "__main__":
+    # This is running in cgi mode
+    import cgitb
+    import sys
+    sys.stderr = sys.stdout
+    cgitb.enable()
+    print("Content-type: text/html\n")
+
+
+if __name__ == "__main__":
+    env = Environment(
+        loader=PackageLoader("peering_form", ""),
+        autoescape=select_autoescape()
+    )
+    template = env.get_template("peers.jinja.html")
+
+    with open("/evix/secret-config.json") as config_f:
+        config = json.load(config_f)
+    database = None
+    try:
+        database = mysql.connector.connect(user=config['database']['user'], password=config['database']['password'],
+                                           host=config['database']['host'], database=config['database']['database'],
+                                           autocommit=True)
+    except mysql.connector.Error as err:
+        print("<pre>Something went wrong with the database connection:")
+        print(err)
+        exit(1)
+    cursor = database.cursor()
+    cursor.execute("""
+        SELECT 
+            json_object(
+                'ip', ip,
+                'asn', asns.asn,
+                'client_id', client_id,
+                'client', json_object(
+                    'name', name,
+                    'website', website,
+                    'connections', (SELECT json_arrayagg(json_object(
+                        'type', type,
+                        'location', server)) FROM connections WHERE connections.client_id=clients.id)))
+        FROM
+            ips INNER JOIN
+            asns on asns.asn=ips.asn
+            INNER JOIN clients ON client_id=id
+        WHERE
+            provisioned
+        ORDER BY
+            IS_IPV6(ip),
+            INET6_ATON(ip)
+    """)
+    print(cursor.fetchone())
+
