6daac09fe6b9956b2b17456ae424dcc5109e99df
Chris <christopher@ve7alb.ca>
2021-05-14 21:03:23 -0700

    Addded full website

--- run/website/render.sh
+++ run/website/render.sh
@@ -2,5 +2,5 @@
 
 /usr/bin/sass /evix/run/website
 /evix/run/website/peers.py > /evix/run/website/peers.html
-/usr/bin/rsync -r -v --delete --del /evix/run/website/ /var/www/evix/new/ \
+/usr/bin/rsync -r -v --delete --del /evix/run/website/ /var/www/evix/ \
   --exclude='static/graphs' --exclude='.well-known' --exclude='participants.json' --exclude='evix.json'
