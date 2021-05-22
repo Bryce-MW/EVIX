9e215416cdb46577a0395750b330fda79023049c
Alexander Bruegmann <mail@abruegmann.eu>
2021-05-22 12:58:20 +0200

    add some more exclusions

--- run/website/render.sh
+++ run/website/render.sh
@@ -2,5 +2,6 @@
 
 /usr/bin/sass /evix/run/website
 /evix/run/website/peers.py > /evix/run/website/peers.html
-/usr/bin/rsync -r -v --delete --del /evix/run/website/ /var/www/evix/ \
-  --exclude='static/graphs' --exclude='.well-known' --exclude='participants.json' --exclude='evix.json'
+/usr/bin/rsync -r -v --delete --delete-during  /evix/run/website/ /var/www/evix/ \
+  --exclude='static/graphs' --exclude='.well-known' --exclude='participants.json' --exclude='evix.json' \
+  --exclude='*.sh' --exclude='peers.jinja.html' --exclude='peers.py' --exclude='peers.jq' --exclude='__pycache__'
