161b828a3ff44c7aba14340c38a76594b0c26001
Chris <christopher@ve7alb.ca>
2021-05-22 05:52:59 -0700

    switch to html example config

--- run/website/render.sh
+++ run/website/render.sh
@@ -2,6 +2,8 @@
 
 /usr/bin/sass /evix/run/website
 /evix/run/website/peers.py > /evix/run/website/peers.html
+/usr/local/bin/mdninja /evix/run/website/example_config.md -o /evix/run/website/example_config.html \
+  --template=/evix/run/website/example_config.jinja.html
 /usr/bin/rsync -r -v --delete --delete-during  /evix/run/website/ /var/www/evix/ \
   --exclude='static/graphs' --exclude='.well-known' --exclude='participants.json' --exclude='evix.json' \
   --exclude='*.sh' --exclude='peers.jinja.html' --exclude='peers.py' --exclude='peers.jq' --exclude='__pycache__' \
